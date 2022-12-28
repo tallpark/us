4bf4801b35c6bf5c94a6d0a946dfd82812ebf25d
tallpark <tallspark@naver.com>
2022-12-28 17:20:05 +0900

    add.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 
-const loginRouter = require('./routers/login.js')
+const loginRouter = require('./routers/login')
+const registerRouter = require('./routers/register')
 
 app.use(express.json());
+app.use("/register", registerRouter)
+app.use("/login", loginRouter)
 
 app.get("/", (req, res) => {
-    res.send("Welcome my page");
+    res.send("Welcome to my page");
 })
 
 app.listen(8080, () => {
     console.log("서버 접속")
 })
-
-asdf
